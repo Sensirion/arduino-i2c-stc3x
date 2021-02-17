37c240d9424c9f97a05b1bd6d000d97f1e8ca847
Bjoern Muntwyler <Bjoern.Muntwyler@sensirion.com>
2021-02-17 22:13:25 +0100

    Add example usage

--- examples/exampleUsage/exampleUsage.ino
+++ examples/exampleUsage/exampleUsage.ino
@@ -1,9 +1,4 @@
 /*
- * I2C-Generator: 0.2.0
- * Yaml Version: 0.1.1
- * Template Version: 0.6.0
- */
-/*
  * Copyright (c) 2021, Sensirion AG
  * All rights reserved.
  *
@@ -37,10 +32,9 @@
 #include <Arduino.h>
 #include <SensirionI2CStc3x.h>
 #include <Wire.h>
+#include <inttypes.h>
 
 SensirionI2CStc3x stc3x;
-
-// TODO: DRIVER_GENERATOR Add missing commands and make printout more pretty
 
 void setup() {
 
@@ -56,68 +50,80 @@ void setup() {
 
     stc3x.begin(Wire);
 
-    uint32_t productNumber;
-    uint8_t serialNumber[8];
-    uint8_t serialNumberSize = 8;
-
-    error = stc3x.readProductIdentifier(productNumber, serialNumber,
-                                        serialNumberSize);
-
+    error = stc3x.prepareProductIdentifier();
     if (error) {
-        Serial.print("Error trying to execute readProductIdentifier(): ");
+        Serial.print("Error trying to execute prepareProductIdentifier(): ");
         errorToString(error, errorMessage, 256);
         Serial.println(errorMessage);
     } else {
-        Serial.print("ProductNumber:");
-        Serial.print(productNumber);
-        Serial.print("\t");
-        Serial.print("SerialNumber:");
-        for (size_t i = 0; i < serialNumberSize; i++) {
-            Serial.print(serialNumber[i]);
-            Serial.print(", ");
+        uint32_t productNumber;
+        uint8_t serialNumberRaw[32];
+        uint8_t serialNumberSize = 32;
+        error = stc3x.readProductIdentifier(productNumber, serialNumberRaw,
+                                            serialNumberSize);
+        if (error) {
+            Serial.print("Error trying to execute readProductIdentifier(): ");
+            errorToString(error, errorMessage, 256);
+            Serial.println(errorMessage);
+        } else {
+            uint64_t serialNumber = (uint64_t)serialNumberRaw[0] << 56 |
+                                    (uint64_t)serialNumberRaw[1] << 48 |
+                                    (uint64_t)serialNumberRaw[2] << 40 |
+                                    (uint64_t)serialNumberRaw[3] << 32 |
+                                    (uint64_t)serialNumberRaw[4] << 24 |
+                                    (uint64_t)serialNumberRaw[5] << 16 |
+                                    (uint64_t)serialNumberRaw[6] << 8 |
+                                    (uint64_t)serialNumberRaw[7];
+            Serial.print("ProductNumber: 0x");
+            Serial.println(productNumber, HEX);
+            char buffer[20];
+            sprintf(buffer, "SerialNumber: %llu", serialNumber);
+            Serial.println(buffer);
         }
-        Serial.println();
     }
 
     uint16_t selfTestOutput;
-
     error = stc3x.selfTest(selfTestOutput);
-
     if (error) {
         Serial.print("Error trying to execute selfTest(): ");
         errorToString(error, errorMessage, 256);
         Serial.println(errorMessage);
     } else {
-        Serial.print("SelfTestOutput:");
-        Serial.println(selfTestOutput);
+        char buffer[32];
+        sprintf(buffer, "Self Test: 0x%04x (OK = 0x0000)", selfTestOutput);
+        Serial.println(buffer);
     }
 
-    // Start Measurement
+    error = stc3x.setBinaryGas(0x0001);
+    if (error) {
+        Serial.print("Error trying to execute selfTest(): ");
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    } else {
+        Serial.println("Set binary gas to 0x0001");
+    }
 }
 
 void loop() {
-    uint16_t error;
-    char errorMessage[256];
-
-    // TODO: DRIVER_GENERATOR Adjust measurement delay
     delay(1000);
-    // TODO: DRIVER_GENERATOR Add scale and offset to printed measurement values
-    // Read Measurement
 
+    // Measure
     uint16_t gasTicks;
     uint16_t temperatureTicks;
 
-    error = stc3x.measureGasConcentration(gasTicks, temperatureTicks);
+    uint16_t error;
+    char errorMessage[256];
 
+    error = stc3x.measureGasConcentration(gasTicks, temperatureTicks);
     if (error) {
         Serial.print("Error trying to execute measureGasConcentration(): ");
         errorToString(error, errorMessage, 256);
         Serial.println(errorMessage);
     } else {
-        Serial.print("GasTicks:");
-        Serial.print(gasTicks);
+        Serial.print("GasConcentration:");
+        Serial.print(100 * ((float)gasTicks - 16384.0) / 32768.0);
         Serial.print("\t");
-        Serial.print("TemperatureTicks:");
-        Serial.println(temperatureTicks);
+        Serial.print("Temperature:");
+        Serial.println((float)temperatureTicks / 200.0);
     }
 }
